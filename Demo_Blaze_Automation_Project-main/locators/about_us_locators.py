56a43ca1df8342ea233e7e1ceaddb131c41e988e
tal2345 <127048321+tal2345@users.noreply.github.com>
2023-03-23 10:16:00 +0200

    Update about_us_locators.py

--- Demo_Blaze_Automation_Project-main/locators/about_us_locators.py
+++ Demo_Blaze_Automation_Project-main/locators/about_us_locators.py
@@ -1,5 +1,5 @@
 
-class About_As_Locators:
+class About_us_Locators:
 
     about_us_btn = "/html/body/nav/div[1]/ul/li[3]/a"
     play_video_btn = "/html/body/div[4]/div/div/div[2]/form/div/div/button"
